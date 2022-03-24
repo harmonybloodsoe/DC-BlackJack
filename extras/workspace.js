7f256f149d3cef22eedf3f3a8ee65b358e07f6b5
harmonyrhi <37599401+harmonyrhi@users.noreply.github.com>
2022-03-24 15:05:26 -0400

    Adds deal function that can be reused

--- extras/workspace.js
+++ extras/workspace.js
@@ -1,15 +1,16 @@
-var playerHand = [];
+var yourHand = [];
 var dealerHand = [];
 var deck = [];
-
+//initalizes variables
 
 function createDeck(){
     class Card {
-        constructor(rank, suit) {
+        constructor(rank, suit, img) {
             this.rank = rank;
             this.suit = suit;
-        }
-    }
+            this.img = `images/${rank}_of_${suit}.png`
+        };
+    };
 
     let suits = ["hearts", "spades", "diamonds", "spades"];
     
@@ -21,6 +22,29 @@ function createDeck(){
     };
 
 };
-
 createDeck();
+
+function fisherYates (myArray) {
+    let i = myArray.length;
+    while (i!==0 && --i) {
+       let j = Math.floor(Math.random() * (i + 1) );
+       let tempi = myArray[i];
+       let tempj = myArray[j];
+       myArray[i] = tempj;
+       myArray[j] = tempi;
+    };
+};
+fisherYates(deck);
+//shuffle deck
+
+function toDeal(player, cardNum){
+    for (let i=0; i < cardNum; i++){
+        let newCard = deck.shift();
+        player.push(newCard);
+    };
+};
+
 console.log(deck);
+toDeal(yourHand, 2);
+console.log(deck);
+console.log(yourHand);
