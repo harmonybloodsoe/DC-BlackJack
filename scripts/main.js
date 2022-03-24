4c59c0825c151a4ca3f2545151889064d939326f
harmonyrhi <37599401+harmonyrhi@users.noreply.github.com>
2022-03-24 09:19:39 -0400

    adds framework for deal function

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
 window.addEventListener('DOMContentLoaded', function() {
   // Execute after page load
+  var playerHand = [];
+  var dealerHand = [];
+  var deck = [];
+
+  const dealButton = document.getElementById('deal-button');
+  dealButton.addEventListener('click', toDeal);
+  function toDeal(){
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 })
