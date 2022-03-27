fe017d85a056dc0616b5a0cd3be0c7d17bef5723
harmonyrhi <37599401+harmonyrhi@users.noreply.github.com>
2022-03-27 19:52:56 -0400

    Adds scoring system

--- extras/workspace.js
+++ extras/workspace.js
@@ -3,6 +3,9 @@ const { type } = require("express/lib/response");
 var yourHand = [];
 var dealerHand = [];
 var deck = [];
+var yourScore= 0;
+var dealerScore= 0; 
+var winner; 
 //initalizes variables
 
 function createDeck(){
@@ -14,7 +17,7 @@ function createDeck(){
         };
     };
 
-    let suits = ["hearts", "spades", "diamonds", "spades"];
+    let suits = ["hearts", "clubs", "diamonds", "spades"];
     
     for (let i = 0; i<suits.length; i++){
         for (let x = 1; x<=13; x++){
@@ -31,31 +34,60 @@ function createDeck(){
             13: 'king', 
         };
         face.img= `images/${faceRanks[face.rank]}_of_${face.suit}.png`;
+        if (face.rank != 1) face.rank=10;
     };
 
    for (let i=0; i<deck.length; i++){  
        if (deck[i].rank==1 || deck[i].rank>10) faceCard(deck[i]);
     }
- 
+    
 };
 createDeck();
+
 
 function fisherYates (myArray) {
     let i = myArray.length;
     while (i!==0 && --i) {
-       let j = Math.floor(Math.random() * (i + 1) );
+       let j = Math.floor(Math.random() * (i+1) );
        let tempi = myArray[i];
        let tempj = myArray[j];
        myArray[i] = tempj;
        myArray[j] = tempi;
     };
 };
+fisherYates(deck);
 
-function toDeal(player, cardNum=2){
+
+function scoreKeep(player, score){
+    player.forEach(element => {
+        if (element.rank == 1){
+            score<11 ? score+=11 : score+=element.rank;
+        }
+        else {
+            score += element.rank;
+        };
+        console.log(score);
+    });
+};
+
+
+function toDeal(player, score, cardNum=2){
     for (let i=0; i < cardNum; i++){
         let newCard = deck.shift();
         player.push(newCard);
     };
+    scoreKeep(player, score);
 };
+toDeal(yourHand, yourScore);
+console.log(yourHand);
+toDeal(dealerHand, dealerScore);
+console.log(dealerHand);
 
-console.log(deck);
+
+if (yourScore == 21 || dealerScore > 21){
+    winner = "player";
+}
+else if (yourScore > 21 || dealerScore == 21){
+    winner = "dealer";
+}
+
